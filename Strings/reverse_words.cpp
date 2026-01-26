4eadb2cff6dae0d9bcc44a99415efa94d41cbbd7
Samuthrika Shree S <samuthrikashrees@gmail.com>
2026-01-26 20:42:32 +0530

    Added comments(brute-force)

---
+++ Strings/reverse_words.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str;
+    cout<<"Enter a string to reverse: ";
+    getline(cin,str);
+    bool isword = false;
+    for(int i=0;i<str.size();i++)
+    {
+
+    }
+}
